2fe473ebb66f095800a3bad899432762a4fd5506
Zerglrisk <wmf1235@naver.com>
2017-05-15 18:20:56 +0900

    version 0.1.0.0
    Add Search Type Compressed File(zip, rar),  Text File(txt, rtf),  Music(mp3),  ebook(ebook, ebooks, book, books), pdf(pdf, chm),  Images(gif, jpeg, jpg, png, bmp, tif, tiff)
    auto detect installed browser and set default browser
    Press Arrow Key(<- ,  ->), You can change Using Browser
    Press Shift + Arrow Key(<-,  ->) You can change File Search Type
    You can Type Character if Keyboard Cursor not on textbox
    Music Fishing Site spats.tokyo, unknownsecret.info, sirens.rocks URL exclude.

---
+++ Google Searcher/BrowserFinder.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+
+public class BrowserFinder
+{
+    /*
+         * Find All Installed Browser
+         * http://stackoverflow.com/questions/2370732/how-to-find-all-the-browsers-installed-on-a-machine
+         */
+    public Dictionary<int, Google_Searcher.MainWindow.BrowseData> getInstalledBrowser()
+    {
+        Google_Searcher.MainWindow.BrowserTotal = 0;
+        Dictionary<int, Google_Searcher.MainWindow.BrowseData> data = new Dictionary<int, Google_Searcher.MainWindow.BrowseData>();
+        
+        using (RegistryKey hklm = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32))
+        {
+            RegistryKey webClientsRootKey = hklm.OpenSubKey(@"SOFTWARE\Clients\StartMenuInternet");
+            if (webClientsRootKey != null)
+                foreach (var subKeyName in webClientsRootKey.GetSubKeyNames())
+                    if (webClientsRootKey.OpenSubKey(subKeyName) != null)
+                        if (webClientsRootKey.OpenSubKey(subKeyName).OpenSubKey("shell") != null)
+                            if (webClientsRootKey.OpenSubKey(subKeyName).OpenSubKey("shell").OpenSubKey("open") != null)
+                                if (webClientsRootKey.OpenSubKey(subKeyName).OpenSubKey("shell").OpenSubKey("open").OpenSubKey("command") != null)
+                                {
+                                    string commandLineUri = (string)webClientsRootKey.OpenSubKey(subKeyName).OpenSubKey("shell").OpenSubKey("open").OpenSubKey("command").GetValue(null);
+                                    //your turn
+
+                                    Google_Searcher.MainWindow.BrowseData temp = new Google_Searcher.MainWindow.BrowseData();
+                                    temp.browserName = (string)webClientsRootKey.OpenSubKey(subKeyName).GetValue(null);
+                                    temp.exePath = commandLineUri;
+                                    data.Add(Google_Searcher.MainWindow.BrowserTotal++, temp);
+                                }
+        }
+        return data;
+    }
+    /*
+     * Find Default Browser
+     * http://stackoverflow.com/questions/13621467/how-to-find-default-web-browser-using-c
+     */
+    public string getDefaultBrowser()
+    {
+        //Open Default Browser
+        //System.Diagnostics.Process.Start("http://www.google.com");
+
+        string browserName = "iexplore";
+        using (RegistryKey userChoiceKey = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice"))
+        {
+            if (userChoiceKey != null)
+            {
+                object progIdValue = userChoiceKey.GetValue("Progid");
+                if (progIdValue != null)
+                {
+                    if (progIdValue.ToString().ToLower().Contains("chrome"))
+                        browserName = "chrome";
+                    else if (progIdValue.ToString().ToLower().Contains("firefox"))
+                        browserName = "firefox";
+                    else if (progIdValue.ToString().ToLower().Contains("safari"))
+                        browserName = "safari";
+                    else if (progIdValue.ToString().ToLower().Contains("opera"))
+                        browserName = "opera";
+                }
+            }
+        }
+        return browserName;
+    }
+
+
+
+    /*
+     * Find A Installed Web Browser.
+     * http://stackoverflow.com/questions/24909108/get-installed-software-list-using-c-sharp
+     */
+    /*public static bool IsApplicationInstalled(string p_name)
+    {
+        string displayName;
+        RegistryKey key;
+
+        // search in: CurrentUser
+        key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall");
+        foreach (String keyName in key.GetSubKeyNames())
+        {
+            RegistryKey subkey = key.OpenSubKey(keyName);
+            displayName = subkey.GetValue("DisplayName") as string;
+            if (p_name.Equals(displayName, StringComparison.OrdinalIgnoreCase) == true)
+            {
+                return true;
+            }
+        }
+
+        // search in: LocalMachine_32
+        key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall");
+        foreach (String keyName in key.GetSubKeyNames())
+        {
+            RegistryKey subkey = key.OpenSubKey(keyName);
+            displayName = subkey.GetValue("DisplayName") as string;
+            if (p_name.Equals(displayName, StringComparison.OrdinalIgnoreCase) == true)
+            {
+                return true;
+            }
+        }
+
+        // search in: LocalMachine_64
+        if(Environment.Is64BitOperatingSystem == true)
+        {
+            key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall");
+            foreach (String keyName in key.GetSubKeyNames())
+            {
+                RegistryKey subkey = key.OpenSubKey(keyName);
+                displayName = subkey.GetValue("DisplayName") as string;
+                if (p_name.Equals(displayName, StringComparison.OrdinalIgnoreCase) == true)
+                {
+                    return true;
+                }
+            }
+        }
+
+
+        // NOT FOUND
+        return false;
+    }*/
+}
--- Google Searcher/MainWindow.xaml.cs
+++ Google Searcher/MainWindow.xaml.cs
@@ -1,17 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Deployment;
+using System.Reflection;
 
 namespace Google_Searcher
 {
@@ -20,9 +15,76 @@ namespace Google_Searcher
     /// </summary>
     public partial class MainWindow : Window
     {
+        //General
+        public static int BrowserTotal;
+        int BrowserIndex;
+        Dictionary<int, BrowseData> Browser;
+        public static int SearchTypeTotal;
+        int SearchTypeIndex;
+        Dictionary<int, SearchData> SearchType;
+
+        public class BrowseData
+        {
+            public string browserName { get; set; }
+            public string exePath { get; set; }
+        }
+
+        public class SearchData
+        {
+            public string SearchContent { get; set; }
+            public string SearchAdditional { get; set; }
+            public List<BrowseData> AdditionalSite { get; set; }
+
+        }
+
+
         public MainWindow()
         {
             InitializeComponent();
+            Browser = new BrowserFinder().getInstalledBrowser();
+            SearchType = new Dictionary<int, SearchData>();
+            string defaultBrowserName = new BrowserFinder().getDefaultBrowser();
+            foreach(var browser in Browser)
+            {
+                string[] spstring = browser.Value.exePath.Split('\\');
+                if(defaultBrowserName.Equals(spstring[spstring.Length - 1].Substring(0, spstring[spstring.Length - 1].Length - (spstring[spstring.Length - 1].Contains('\"') ? 5 : 4)))){
+                    BrowserIndex = browser.Key;
+                    break;
+                }
+            }
+            Label_Browser.Content = Browser[BrowserIndex].browserName;
+
+            SearchData temp = new SearchData();
+            temp.SearchContent = "Music";
+            temp.SearchAdditional = " -html -htm -php -shtml -opendivx -md5 -md5sums -mp3free4 intitle:Index.of (mp3)  -site:spats.tokyo -site:unknownsecret.info -site:sirens.rocks"; // -xxx
+            SearchType.Add(0, temp);
+            temp = new SearchData();
+            temp.SearchContent = "Images";
+            temp.SearchAdditional = " -html -htm -php -shtml -opendivx -md5 -md5sums intitle:index.of (gif|jpeg|jpg|png|bmp|tif|tiff)"; // -xxx
+            SearchType.Add(1, temp);
+            temp = new SearchData();
+            temp.SearchContent = "eBook";
+            temp.SearchAdditional = " -html -htm -php -shtml -opendivx -md5 -md5sums intitle:index.of (/ebook|/ebooks|/book|/books)"; // -xxx
+            SearchType.Add(2, temp);
+            temp = new SearchData();
+            temp.SearchContent = "Pdf";
+            temp.SearchAdditional = " -html -htm -php -shtml -opendivx -md5 -md5sums intitle:index.of (chm|pdf)"; // -xxx
+            SearchType.Add(3, temp);
+            temp = new SearchData();
+            temp.SearchContent = "Text File";
+            temp.SearchAdditional = " -html -htm -php -shtml -opendivx -md5 -md5sums intitle:index.of (txt|rtf)"; // -xxx
+            SearchType.Add(4, temp);
+            temp = new SearchData();
+            temp.SearchContent = "Compressed File";
+            temp.SearchAdditional = " -html -htm -php -shtml -opendivx -md5 -md5sums intitle:index.of (zip|rar)"; // -xxx
+            SearchType.Add(5, temp);
+
+            SearchTypeIndex = 0;
+            SearchTypeTotal = 6;
+
+
+            Label_Version.Content = "Version " + getRunningVersion();
+
         }
 
         private void Label_Caption_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -44,12 +106,14 @@ namespace Google_Searcher
 
         private void Browser_Left_Click(object sender, RoutedEventArgs e)
         {
-            
+            BrowserIndex = (BrowserIndex == 0 ? BrowserTotal - 1 : BrowserIndex - 1);
+            Label_Browser.Content = Browser[BrowserIndex].browserName;
         }
 
         private void Browser_Right_Click(object sender, RoutedEventArgs e)
         {
-
+            BrowserIndex = (BrowserIndex == BrowserTotal -1 ? 0 : BrowserIndex + 1);
+            Label_Browser.Content = Browser[BrowserIndex].browserName;
         }
 
         private void Browser_Add_Click(object sender, RoutedEventArgs e)
@@ -64,12 +128,14 @@ namespace Google_Searcher
 
         private void Search_Type_Left_Click(object sender, RoutedEventArgs e)
         {
-            
+            SearchTypeIndex = (SearchTypeIndex == 0 ? SearchTypeTotal - 1 : SearchTypeIndex - 1);
+            Label_Search_Type.Content = SearchType[SearchTypeIndex].SearchContent;
         }
 
         private void Search_Type_Right_Click(object sender, RoutedEventArgs e)
         {
-
+            SearchTypeIndex = (SearchTypeIndex == SearchTypeTotal - 1 ? 0 : SearchTypeIndex + 1);
+            Label_Search_Type.Content = SearchType[SearchTypeIndex].SearchContent;
         }
 
         private void Search_Type_Add_Click(object sender, RoutedEventArgs e)
@@ -86,6 +152,46 @@ namespace Google_Searcher
 
         }
 
-        
+        private void btn_Search_Click(object sender, RoutedEventArgs e)
+        {
+            string str;
+            str = "https://www.google.com/search?q=" + textBox.Text;
+            str += SearchType[SearchTypeIndex].SearchAdditional;
+            Process.Start(new ProcessStartInfo(Browser[BrowserIndex].exePath, str.Replace(' ', '+')));
+        }
+
+        private void window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (textBox.Focus() == false)
+            {
+                textBox.Focus();
+                textBox.Text = e.Key.ToString();
+            }
+            else if (textBox.Focus() == true && e.Key == Key.Enter)
+                btn_Search.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+
+            if (e.Key == Key.Left)
+            {
+                if (Keyboard.IsKeyDown(Key.LeftShift))
+                    Search_Type_Left.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+                else
+                    Browser_Left.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+            }
+            if (e.Key == Key.Right)
+            {
+                if (Keyboard.IsKeyDown(Key.LeftShift))
+                    Search_Type_Right.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+                else
+                    Browser_Right.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+            }
+            //http://stackoverflow.com/questions/257587/bring-a-window-to-the-front-in-wpf
+        }
+
+        //https://msftstack.wordpress.com/2015/12/30/how-to-display-the-version-in-a-c-wpf-app/
+        private Version getRunningVersion()
+        {
+            return Assembly.GetExecutingAssembly().GetName().Version;
+            
+        }
     }
 }
